21c75699b7b6faca6f53d37b4abb47c8634e023f
Zachary Orr <zachary.orr@gmail.com>
2020-07-19 00:22:10 -0400

    Added to config

--- app/__init__.py
+++ app/__init__.py
@@ -25,7 +25,7 @@ app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) if app.conf
 
 from app import routes, models, errors
 
-if not app.debug:
+if not app.debug and not app.testing:
     if app.config['MAIL_SERVER']:
         auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
@@ -49,6 +49,24 @@ if not app.debug:
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
+
+    if app.config['LOG_TO_STDOUT']:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/microblog.log',
+                                           maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s '
+            '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('HomieList startup')
